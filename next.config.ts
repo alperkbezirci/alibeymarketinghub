54ac0df462722fade6a095809f6e126bcbfe1e06
Alper Küçükbezirci <alperkbezirci@gmail.com>
2025-06-05 21:37:03 +0000

    Blocked cross-origin request from 3000-firebase-studio-1748891728364.clu

--- next.config.ts
+++ next.config.ts
@@ -26,7 +26,10 @@ const nextConfig: NextConfig = {
   },
   // Add the Firebase Studio preview origin to allowedDevOrigins
   // to prevent cross-origin request errors during development.
-  allowedDevOrigins: ['https://9000-firebase-studio-1748891728364.cluster-jbb3mjctu5cbgsi6hwq6u4btwe.cloudworkstations.dev'],
+  allowedDevOrigins: [
+    'https://9000-firebase-studio-1748891728364.cluster-jbb3mjctu5cbgsi6hwq6u4btwe.cloudworkstations.dev',
+    'https://3000-firebase-studio-1748891728364.cluster-jbb3mjctu5cbgsi6hwq6u4btwe.cloudworkstations.dev'
+  ],
 };
 
 export default nextConfig;
